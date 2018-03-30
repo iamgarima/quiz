c657455b6c61bd91714900b9345876150ad9376d
iamgarima <garima.kamboj@icloud.com~>
2018-03-30 15:38:18 +0530

    testing lint

--- index.js
+++ index.js
@@ -23,7 +23,6 @@ app.use(
     })
 );
 app.use(passport.session());
-
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log(`Listening on port ${port}`)); // eslint-disable-line no-console
