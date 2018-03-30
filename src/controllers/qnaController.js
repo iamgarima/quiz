86e80e58921ab01af6e51a10db474d26cd5057f0
iamgarima <garima.kamboj@icloud.com~>
2018-03-30 15:20:40 +0530

    Update questions models and controllers to async/await version

--- src/controllers/qnaController.js
+++ src/controllers/qnaController.js
@@ -1,31 +1,39 @@
 const Quiz = require("../models/questions");
 
-const generateRandomQidList = n =>
-    Quiz.getRowCount("questions")
-        .then(count => {
-            const randomList = [];
-            while (randomList.length !== n) {
-                const val = Math.ceil(Math.random() * count);
-                if (randomList.indexOf(val) === -1) {
-                    randomList.push(val);
-                }
+const generateRandomQidList = async n => {
+    try {
+        const count = await Quiz.getRowCount("questions");
+        const randomList = [];
+        while (randomList.length !== n) {
+            const val = Math.ceil(Math.random() * count);
+            if (randomList.indexOf(val) === -1) {
+                randomList.push(val);
             }
-            return randomList;
-        })
-        .catch(err => console.log("Error")); // eslint-disable-line no-console
-
-const getQuestions = (req, res) => {
-    generateRandomQidList(req.body.count).then(qIdList => {
-        Quiz.getQuestionsWithOptions(qIdList)
-            .then(result => res.send(result))
-            .catch(err => res.send(500));
-    });
+        }
+        return randomList;
+    } catch (err) {
+        console.log("Error while fetching questions count"); // eslint-disable-line no-console
+        throw err;
+    }
 };
 
-const getAnswers = (req, res) => {
-    Quiz.getAnswersForQuestions(req.body.qIdList)
-        .then(result => res.send(result))
-        .catch(err => res.send(500));
+const getQuestions = async (req, res) => {
+    try {
+        const qIdList = await generateRandomQidList(req.body.count);
+        const result = await Quiz.getQuestionsWithOptions(qIdList);
+        res.send(result);
+    } catch (err) {
+        res.sendStatus(500);
+    }
+};
+
+const getAnswers = async (req, res) => {
+    try {
+        const result = await Quiz.getAnswersForQuestions(req.body.qIdList);
+        res.send(result);
+    } catch (err) {
+        res.send(500);
+    }
 };
 
 module.exports = {
--- src/models/questions.js
+++ src/models/questions.js
@@ -1,112 +1,98 @@
 const db = require("../db");
 
-const getQuestion = id =>
-    new Promise((resolve, reject) => {
-        db
-            .query("Select * from questions where id=$1", [id])
-            .then(result => resolve(result.rows[0]))
-            .catch(err => {
-                console.log("Error happened in getQuestion: ", err);  // eslint-disable-line no-console
-                reject(err);
+const getQuestion = async id => {
+    try {
+        const result = await db.query("Select * from questions where id=$1", [
+            id
+        ]);
+        return result.rows[0];
+    } catch (err) {
+        console.log("Error happened in getQuestion: ", err); // eslint-disable-line no-console
+        throw err;
+    }
+};
+
+const getAnswer = async qId => {
+    try {
+        const result = await db.query("Select * from solutions where qId=$1", [
+            qId
+        ]);
+        return result.rows[0];
+    } catch (err) {
+        console.log("Error happened in getAnswers model: ", err); // eslint-disable-line no-console
+        throw err;
+    }
+};
+
+const getRowCount = async tableName => {
+    try {
+        const count = await db.query(`select count(*) from ${tableName};`, []);
+        return Number(count.rows[0].count);
+    } catch (err) {
+        console.log("Error happened in getRowCount: ", err); // eslint-disable-line no-console
+        throw err;
+    }
+};
+
+const getOptionText = async optionId => {
+    try {
+        const optionText = await db.query("Select * from options where id=$1", [
+            optionId
+        ]);
+        return optionText.rows[0];
+    } catch (err) {
+        console.log("Error happened in getOptionText: ", err); // eslint-disable-line no-console
+        throw err;
+    }
+};
+
+const getQuestionsWithOptions = async qIdList => {
+    const quesListExec = [];
+    qIdList.forEach(qId => quesListExec.push(getQuestion(qId)));
+    try {
+        const quesList = await Promise.all(quesListExec);
+        const updatedQuesListExec = [];
+        quesList.forEach(ques => {
+            const optionsTextListExec = [];
+            const updatedQues = new Promise((resolve, reject) => {
+                ques.options.forEach(optionId =>
+                    optionsTextListExec.push(getOptionText(optionId))
+                );
+                Promise.all(optionsTextListExec)
+                    .then(optionsTextList => {
+                        ques.options = optionsTextList;
+                        resolve(ques);
+                    })
+                    .catch(err => {
+                        reject(err);
+                    });
             });
-    });
+            updatedQuesListExec.push(updatedQues);
+        });
+        const updatedQuesList = await Promise.all(updatedQuesListExec);
+        return updatedQuesList;
+    } catch (err) {
+        console.log(                                                   // eslint-disable-line no-console
+            "Error happened in getQuestionswithOptions model: ",
+            err
+        );
+        throw err;
+    }
+};
 
-const getAnswer = qId =>
-    new Promise((resolve, reject) => {
-        db
-            .query("Select * from solutions where qId=$1", [qId])
-            .then(result => resolve(result.rows[0]))
-            .catch(err => {
-                // need to give catch either in model or controller
-                console.log("Error happened in getAnswers model: ", err);  // eslint-disable-line no-console
-                reject(err);
-            });
-    });
+const getAnswersForQuestions = async qIdList => {
+    const promList = [];
 
-const getRowCount = tableName =>
-    new Promise((resolve, reject) => {
-        db
-            .query(`select count(*) from ${tableName};`, [])
-            .then(count => resolve(Number(count.rows[0].count)))
-            .catch(err => {
-                console.log("Error happened in getRowCount: ", err);  // eslint-disable-line no-console
-                reject(err);
-            });
-    });
+    qIdList.forEach(qId => promList.push(getAnswer(qId)));
 
-const getOptionText = optionId =>
-    new Promise((resolve, reject) => {
-        db
-            .query("Select * from options where id=$1", [optionId])
-            .then(optionText => resolve(optionText.rows[0]))
-            .catch(err => {
-                console.log("Error happened in getOptionText: ", err);  // eslint-disable-line no-console
-                reject(err);
-            });
-    });
-
-const getQuestionsWithOptions = qIdList =>
-    new Promise((resolve, reject) => {
-        const quesListExec = [];
-
-        qIdList.forEach(qId => quesListExec.push(getQuestion(qId)));
-
-        Promise.all(quesListExec)
-            .then(quesList => {
-                const updatedQuesListExec = [];
-                quesList.forEach(ques => {
-                    const optionsTextListExec = [];
-                    const updatedQues = new Promise(
-                        (resolveInner, rejectInner) => {
-                            ques.options.forEach(optionId =>
-                                optionsTextListExec.push(
-                                    getOptionText(optionId)
-                                )
-                            );
-
-                            Promise.all(optionsTextListExec)
-                                .then(optionsTextList => {
-                                    ques.options = optionsTextList;
-                                    resolveInner(ques);
-                                })
-                                .catch(err => {
-                                    rejectInner(err);
-                                });
-                        }
-                    );
-
-                    updatedQuesListExec.push(updatedQues);
-                });
-
-                Promise.all(updatedQuesListExec)
-                    .then(updatedQuesList => resolve(updatedQuesList))
-                    .catch(err => reject(err));
-            })
-            .catch(err => {
-                console.log(                                                    // eslint-disable-line no-console
-                    "Error happened in getQuestionswithOptions model: ",
-                    err
-                ); 
-                reject(err);
-            });
-    });
-
-const getAnswersForQuestions = qIdList =>
-    new Promise((resolve, reject) => {
-        const promList = [];
-
-        qIdList.forEach(qId => promList.push(getAnswer(qId)));
-
-        Promise.all(promList)
-            .then(ansList => resolve(ansList))
-            .catch(err => {
-                console.log(                                                // eslint-disable-line no-console
-                    "Error happened in getAnswersForQuestions model: ",
-                    err
-                ); 
-                reject(err);
-            });
-    });
+    try {
+        const ansList = await Promise.all(promList);
+        return ansList;
+    } catch (err) {
+        console.log("Error happened in getAnswersForQuestions model: ", err); // eslint-disable-line no-console
+        throw err;
+    }
+};
 
 module.exports = {
     getAnswer,
