fc5189cf273140e8f59a4e97fba6c5378d3ef9b9
iamgarima <garima.kamboj@icloud.com~>
2018-04-02 09:46:08 +0530

    Add connection string for heroku-postgresql

--- src/db/index.js
+++ src/db/index.js
@@ -1,11 +1,8 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-    user: "postgres",
-    password: "gk",
-    host: "localhost",
-    port: "5432",
-    database: "quiz"
+    connectionString: process.env.DATABASE_URL,
+    ssl: true
 });
 
 const query = (text, params) => pool.query(text, params);
